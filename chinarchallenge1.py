83d076c31bb48886a3e6f4e595eeef9ed53ada2a
Chinar Dingankar <chinar.dingankar@users.noreply.github.com>
2021-10-26 15:31:53 +0000

    copying files and folders

---
+++ chinarchallenge1.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+dogs = ["fido", "spot", "lucky"]
+
+cats = ["fluffy", "snowball", "garfield"]
+
+pets = [];
+
+pets.extend(dogs)
+pets.extend(cats)
+
+print(pets)
+print(dogs)
+
+my_pets = {}
+for key in dogs:
+   for value in cats:
+      my_pets[key] = value
+      cats.remove(value)
+      break
+print("Dictionary from lists :\n ",my_pets)
+
+print("I only own " +  pets[1] +" and " + pets[4]) 
+
+# "I want to adopt fido and garfield too"
+
+print("I want to adopt " + my_pets + " and " )
---
+++ copycat01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import shutil
+import os
+os.chdir('/home/student/mycode/')
+ # The following line will copy file
+shutil.copy('5g_research/sdn_network.txt', '5g_research/sdn_network.txt.copy')
+ # The following line will create the directory if it does not exist already
+shutil.copytree('5g_research/', '5g_research_backup/')
+
+
